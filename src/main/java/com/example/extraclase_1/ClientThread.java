816c811eb27a82335f95ae5e06b09b7fd349e31e
StephanieChaves <stchaves@estudiantec.cr>
2023-08-25 21:17:48 -0600

    Client

---
+++ src/main/java/com/example/extraclase_1/ClientThread.java
@@ -0,0 +1,34 @@
+package com.example.extraclase_1;
+
+import javafx.scene.control.TextArea;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class ClientThread extends Thread{
+    private DatagramSocket socket;
+
+    private byte[] incoming =new byte[256];
+
+    private TextArea textArea;
+
+    public ClientThread(DatagramSocket socket, TextArea textArea) {
+        this.socket=socket;
+        this.textArea=textArea;
+    }
+    public void run(){
+        System.out.println("iniciando");
+        while (true) {
+            DatagramPacket packet= new DatagramPacket(incoming, incoming.length);
+            try {
+                socket.receive(packet);
+            }catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            String message=new String(packet.getData(), 0, packet.getLength()) + "\n";
+            String current=textArea.getText();
+            textArea.setText(current+message);
+        }
+    }
+}
