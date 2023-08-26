5542bbb7a1cb961d1c774b3d9fd15a30a0f8ad73
StephanieChaves <stchaves@estudiantec.cr>
2023-08-25 23:32:50 -0600

    Cliente Final, ya que el otro tiene errores

--- src/main/java/com/example/extraclase_1/ChatClient.java
+++ src/main/java/com/example/extraclase_1/ChatClient.java
@@ -38,34 +38,71 @@ public class ChatClient extends Application {
         }
     }
     private static final InetAddress address;
+/**
+ *Se declara una variable llamada address con los modificadores private static final
+ */
 
     static {
         try {
             address= InetAddress.getByName("localhost");
+            /**
+             *Dentro del bloque estático, se intenta obtener una instancia de InetAddress
+             * que representa la dirección IP asociada al nombre de host "localhost".
+             * InetAddress se utiliza para representar direcciones IP y nombres de host.
+             * El método getByName(String host) se utiliza para obtener una instancia de
+             * InetAddress a partir del nombre de host.
+             */
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
+            /**
+             * Manejo de excepciones, maneja la excepción que no se pueda resolver la dirección IP para "localhost".
+             */
         }
     }
     private static final String identifier ="usuario";
+    /**
+     * se está declarando una variable estática final llamada identifier y se le asigna
+     * el valor de la cadena "usuario".
+     */
 
     private static final int SERVER_PORT= 8000;
-
+    /**
+     * La intención de esta variable es indicar el número de puerto que se
+     * utilizará para la comunicación con el servidor
+     */
     private static final TextArea messageArea = new TextArea();
 
     private static final TextField inputBox = new TextField();
+    /**
+     * Dos objetos gráficos utilizados en la interface de usuario en JavaFX: messageArea y inputBox
+     */
 
     public static void main(String[] args)throws IOException {
-
-        // thread for receiving messages
+        /**
+         * Método main La declaración throws IOException indica que el método puede lanzar excepciones del
+         * tipo IOException, que está relacionado con operaciones de entrada y salida, como la lectura y
+         * escritura de datos
+         */
         ClientThread clientThread = new ClientThread(socket, messageArea);
         clientThread.start();
+/**
+ * Se está creando una instancia de ClientThread y pasan como argumentos el socket (socket) y el área de
+ * mensajes (messageArea). Luego, el hilo se inicia con el método start().
+ */
 
-        // send initialization message to the server
         byte[] uuid = ("init;" + identifier).getBytes();
         DatagramPacket initialize = new DatagramPacket(uuid, uuid.length, address, SERVER_PORT);
         socket.send(initialize);
+        /**
+         * Se crea un mensaje de inicialización en forma de arreglo de bytes. Luego, se crea un DatagramPacket
+         * utilizando este arreglo de bytes, la dirección IP (address), el número de puerto del servidor
+         * (SERVER_PORT), y se envía a través del socket.
+         */
 
         launch();
+        /**
+         * función se llama para iniciar la aplicación gráfica
+         */
     }
 
 
@@ -73,29 +110,43 @@ public class ChatClient extends Application {
 
             messageArea.setMaxWidth(500);
             messageArea.setEditable(false);
+        /**
+         * Configuración y creación de la interfaz gráfica de una aplicación JavaFX.
+         */
 
 
             inputBox.setMaxWidth(500);
             inputBox.setOnKeyPressed(event -> {
                 if (event.getCode() == KeyCode.ENTER) {
-                    String temp = identifier + ";" + inputBox.getText(); // message to send
-                    messageArea.setText(messageArea.getText() + inputBox.getText() + "\n"); // update messages on screen
-                    byte[] msg = temp.getBytes(); // convert to bytes
-                    inputBox.setText(""); // remove text from input box
+                    String temp = identifier + ";" + inputBox.getText();
+                    messageArea.setText(messageArea.getText() + inputBox.getText() + "\n");
+                    byte[] msg = temp.getBytes();
+                    inputBox.setText("");
+/**
+ * Manejo de teclas: se verifica si la tecla presionada es la tecla "Enter". Si es así, se
+ * ejecuta el bloque de código que sigue. Esto indica que el código a continuación se ejecutará
+ * cuando se presione la tecla enter en el campo inputBox. y también la creación y manipulación de mensajes.
+ */
 
-                    // create a packet & send
                     DatagramPacket send = new DatagramPacket(msg, msg.length, address, SERVER_PORT);
                     try {
                         socket.send(send);
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
+                    /**
+                     * Manejo de excepciones.
+                     */
                 }
             });
-            // put everything on screen
+
             Scene scene = new Scene(new VBox(35, messageArea, inputBox), 550, 300);
             primaryStage.setScene(scene);
             primaryStage.show();
+        /**
+         *  crea la escena principal de la interfaz gráfica de la aplicación JavaFX y establece esa
+         *  escena en la ventana principal (primaryStage) para mostrarla al usuario.
+         */
         }
     }
 
--- src/main/java/com/example/extraclase_1/ClientThread.java
+++ src/main/java/com/example/extraclase_1/ClientThread.java
@@ -8,27 +8,56 @@ import java.net.DatagramSocket;
 
 public class ClientThread extends Thread{
     private DatagramSocket socket;
+    /**
+     * Se está declarando una clase llamada ClientThread que extiende la clase Thread, clase diseñada para trabajar
+     * para ejecutarse como un hilo separado. Además se está declarando una variable de instancia privada llamada
+     * socket.
+     */
+
 
     private byte[] incoming =new byte[256];
-
+    /**
+     * Está declarando una variable de instancia llamada incoming, que es un arreglo de bytes con
+     * una longitud de 256 elementos.
+     */
     private TextArea textArea;
 
     public ClientThread(DatagramSocket socket, TextArea textArea) {
         this.socket=socket;
         this.textArea=textArea;
+        /**
+         * Se crea un constructor para la clase ClientThread,donde se establecen
+         * los valores iniciales para las variables de instancia socket y textArea.
+         */
     }
     public void run(){
         System.out.println("iniciando");
         while (true) {
             DatagramPacket packet= new DatagramPacket(incoming, incoming.length);
+            /**
+             * Se crea un nuevo objeto DatagramPacket llamado packet que se utilizará
+             * para recibir los datos entrantes. Se pasa el arreglo de bytes incoming y
+             * su longitud como parámetros al constructor.
+             */
             try {
                 socket.receive(packet);
             }catch (IOException e) {
                 throw new RuntimeException(e);
             }
+            /**
+             * Se utiliza un bloque try-catch para manejar la excepción que podría ocurrir
+             * al recibir un paquete a través del socket.
+             */
             String message=new String(packet.getData(), 0, packet.getLength()) + "\n";
+            /**
+             *  Esto crea una cadena message que contiene el contenido del paquete como texto.
+             */
             String current=textArea.getText();
             textArea.setText(current+message);
+            /**
+             *  Esto agrega el mensaje recibido al final del contenido existente del área
+             *  de texto, haciendo que los mensajes se muestren uno tras otro.
+             */
         }
     }
 }
